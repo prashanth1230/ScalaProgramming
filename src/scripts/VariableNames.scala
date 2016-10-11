739fc29ae2155cb8e55241cc065c54a25b0379fa
prashanth1230 <prashanth1230@gmail.com>
2016-10-11 16:15:05 +0530

    Variable name variations in Scala.

---
+++ src/scripts/VariableNames.scala
@@ -0,0 +1,11 @@
+//Variable names can have spaces
+val `my variable name with spaces` = 20
+println(`my variable name with spaces`)
+
+//Variable names can have special characters with an underscore in the beginning
+val isThisMySalary_? = 200
+println(isThisMySalary_?)
+
+//Back ticks can be used even to use reserved key words
+val `import` = "Using a reserved keyword"
+println(`import`)
