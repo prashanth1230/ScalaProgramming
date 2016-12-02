0c70025a8474ce909700b4feeb7f08cbd2a7fa65
Udupa, Prashanth <prashanth.udupa@fmr.com>
2016-12-02 18:34:45 +0530

    More String interpolation.

--- src/scripts/StringInterpolation.scala
+++ src/scripts/StringInterpolation.scala
@@ -1,6 +1,12 @@
 import java.time.LocalDate
 
+//s  interpolator
 val name = "Prashanth"
 println(s"Hi ${name}!");
 
 println(s"My age is ${LocalDate.now().getYear - 1988}")
+
+//f interpolator
+val ticketCost = 50
+val bandName = "Some Band"
+println(f"$bandName%s is paying for the ticket cost $$$ticketCost%1.2f")
