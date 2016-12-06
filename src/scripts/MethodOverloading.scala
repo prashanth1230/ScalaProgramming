8c1182f8d66320b261a9de9619d016f762e9a34d
Udupa, Prashanth <prashanth.udupa@fmr.com>
2016-12-06 14:06:42 +0530

    Overloading.

---
+++ src/scripts/MethodOverloading.scala
@@ -0,0 +1,15 @@
+/**
+  * Created by a487037 on 12/06/2016.
+  */
+
+def add(a: Int, b: Int): Int = {
+  println("Using int version")
+  return a + b
+}
+def add(a: Double, b: Double): Double = {
+  println("Using double version")
+  return a + b
+}
+
+println(add(1, 2))
+println(add(1.3, 2.5))
---
+++ src/scripts/OperatorOverloading.scala
@@ -0,0 +1,7 @@
+/**
+  * Created by a487037 on 12/06/2016.
+  */
+
+def +(a: Int, b: Int) = a + b
+
+println(3.+(10))
