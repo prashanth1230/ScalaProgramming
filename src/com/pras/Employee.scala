dfe9a249057a2f84da592a878d88f5abc6aa645c
Prashanth Udupa <prashanth1230@gmail.com>
2017-01-03 21:29:04 +0530

    Added a val accessor to be able to access the classes' member variables.

--- src/com/pras/Employee.scala
+++ src/com/pras/Employee.scala
@@ -3,8 +3,8 @@ package com.pras
 /**
   * Created by Prashanth on 11-10-2016.
   */
-class Employee(firstName: String, lastName: String, department: Department) {
+class Employee(val firstName: String, val lastName: String, department: Department) {
 
 }
 
-class Department(name: String)
+class Department(val name: String)
