67c42d9433c6399c9b8be04fb8a1d285a26d4c3b
Prashanth Udupa <prashanth1230@gmail.com>
2017-01-04 12:46:20 +0530

    Added constructor variations.

--- src/com/pras/Employee.scala
+++ src/com/pras/Employee.scala
@@ -3,8 +3,9 @@ package com.pras
 /**
   * Created by Prashanth on 11-10-2016.
   */
-class Employee(val firstName: String, val lastName: String, department: Department) {
-
+class Employee(val firstName: String, val lastName: String, val department: String) {
+  //Creating another ancillary constructor
+  def this(firstName: String, lastName: String) = this(firstName, lastName, "Imaging")
 }
 
 class Department(val name: String)
---
+++ src/com/pras/EmployeeScript.scala
@@ -0,0 +1,8 @@
+import com.pras.Employee
+
+/**
+  * Created by Prashanth Udupa on 04-01-2017.
+  */
+
+var prashanth = new Employee("Prashanth", "Udupa")
+println(prashanth.firstName)
