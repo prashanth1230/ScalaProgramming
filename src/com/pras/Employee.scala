6cf68f22e8ce13ab21a6c62a76c8794406ce5583
Prashanth Udupa <prashanth1230@gmail.com>
2017-01-04 13:09:56 +0530

    Example of creating an immutable modifier method.

--- src/com/pras/Employee.scala
+++ src/com/pras/Employee.scala
@@ -7,6 +7,13 @@ package com.pras
 class Employee(val firstName: String, val lastName: String, val department: Department = new Department("Imaging")) {
   //Creating another ancillary constructor
   def this(firstName: String, lastName: String) = this(firstName, lastName, new Department("Imaging"))
+
+  //Methods inside a class
+  def fullName = s"$firstName $lastName"
+
+  //Create a mutator method to change the last name. Note that it does not change the value, but creates a copy
+  //The class is still immutable
+  def changeLastName(argLastName: String) = new Employee(firstName, argLastName, department)
 }
 
 class Department(val name: String)
--- src/com/pras/EmployeeScript.scala
+++ src/com/pras/EmployeeScript.scala
@@ -4,5 +4,11 @@ import com.pras.Employee
   * Created by Prashanth Udupa on 04-01-2017.
   */
 
-var prashanth = new Employee("Prashanth", "Udupa")
+val prashanth = new Employee("Prashanth", "Udupa")
 println(prashanth.firstName)
+
+val newPrashanth = prashanth.changeLastName("Manjunath")
+println(newPrashanth.lastName)
+
+//If the compiled classes are cached, run the command with -nc switch
+//scala -nc com\pras\EmployeeScript.scala
