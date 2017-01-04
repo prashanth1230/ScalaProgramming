7c0cfdeff785e63f24e883e01d8a5d0821a2b75c
Prashanth Udupa <prashanth1230@gmail.com>
2017-01-04 12:57:34 +0530

    Added default parameter to the constructor.

--- src/com/pras/Employee.scala
+++ src/com/pras/Employee.scala
@@ -3,9 +3,10 @@ package com.pras
 /**
   * Created by Prashanth on 11-10-2016.
   */
-class Employee(val firstName: String, val lastName: String, val department: String) {
+//Added a default value to the constructor
+class Employee(val firstName: String, val lastName: String, val department: Department = new Department("Imaging")) {
   //Creating another ancillary constructor
-  def this(firstName: String, lastName: String) = this(firstName, lastName, "Imaging")
+  def this(firstName: String, lastName: String) = this(firstName, lastName, new Department("Imaging"))
 }
 
 class Department(val name: String)
