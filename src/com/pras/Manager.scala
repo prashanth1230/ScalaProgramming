1a64e189b8206c0d17aece6a70717965271df54d
Prashanth Udupa <prashanth1230@gmail.com>
2017-01-05 19:41:18 +0530

    Overriding.

--- src/com/pras/Manager.scala
+++ src/com/pras/Manager.scala
@@ -3,4 +3,7 @@ package com.pras
 /**
   * Created by Prashanth.Manjunath on 04-01-2017.
   */
-class Manager(firstName: String, lastName: String, department: Department, val reportees: List[Employee]) extends Employee(firstName: String, lastName: String, department: Department)
+class Manager(firstName: String, lastName: String, department: Department, val reportees: List[Employee]) extends Employee(firstName: String, lastName: String, department: Department) {
+  //Overriding methods
+  override def fullName: String = s"$firstName $lastName in the department ${department.name}"
+}
